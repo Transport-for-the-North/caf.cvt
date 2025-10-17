8dd43f234ead7a3c3e277419fb3e5fc9cf06776a
DanielJacksonTfN <daniel.jackson@transportforthenorth.com>
2025-10-17 15:20:17 +0100

    Updated data exploration and cleaning.

--- src/cvt/data_cleaning.py
+++ src/cvt/data_cleaning.py
@@ -3,3 +3,19 @@
 import pandas as pd
 import geopandas as gpd
 
+def clip_to_boundary(gdf, boundary):
+    '''Takes a GeoDataFrame and a boundary and returns a GeoDataFrame'''
+    boundary = boundary.to_crs(gdf.crs) # Match CRS
+    gdf_boundary = gpd.clip(gdf, boundary) # Clip GDF to boundary
+    return gdf_boundary
+
+def standard_cleaning(gdf, columns_to_keep):
+    '''Takes a GeoDataFrame, performs standard cleaning operations, and returns a GeoDataFrame'''
+    gdf = gdf.drop_duplicates() # Drop duplicate rows
+    gdf = gdf[columns_to_keep] # Filter for relevant columns
+
+    # Remove rows with empty or null geometry
+    gdf = gdf[~gdf.geometry.is_empty]
+    gdf = gdf[gdf.geometry.notnull()]
+
+
