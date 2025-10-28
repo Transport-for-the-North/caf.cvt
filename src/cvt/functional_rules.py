485bbfb25585fd6b39dc389c4cf4406b8e6cb6fe
DanielJacksonTfN <daniel.jackson@transportforthenorth.com>
2025-10-28 11:41:38 +0000

    Updated data exploration and cleaning. Start of functional rules processing.

---
+++ src/cvt/functional_rules.py
@@ -0,0 +1,3 @@
+'''This script is for applying functional rules
+to each hazard dataset in order to classify the raw data into actionable risk factors'''
+
---
+++ src/cvt/load_data.py
@@ -0,0 +1,26 @@
+'''In this script, we define the file paths for the various datasets we will be loading throughout the project'''
+
+from pathlib import Path
+
+# OTHER DATASETS
+
+BOUNDARY_PATH = Path("F:/") / "4. Climate Datasets" / "Climate Vulnerability Tool" / "model inputs" / "Other" /  "TfN Boundary" / "Transport_for_the_north_boundary_2020_generalised.shp"
+
+# HAZARD DATASETS
+
+SOIL_PATH = Path("F:/") / "4. Climate Datasets" / "Climate Vulnerability Tool" / "model inputs" / "Hazards" / ""
+
+NCERM_PATH = Path("F:/") / "4. Climate Datasets" / "Climate Vulnerability Tool" / "model inputs" / "Hazards" / ""
+
+BGS_SS_2070_PATH = Path("F:/") / "4. Climate Datasets" / "Climate Vulnerability Tool" / "model inputs" / "Hazards" / ""
+
+SUM_MAX_TEMP_PROJ_PATH = Path("F:/") / "4. Climate Datasets" / "Climate Vulnerability Tool" / "model inputs" / "Hazards" / "Extreme Weather"
+
+WIN_MIN_TEMP_PROJ_PATH = Path("F:/") / "4. Climate Datasets" / "Climate Vulnerability Tool" / "model inputs" / "Hazards" / "Extreme Weather"
+
+SUM_PRECIP_CHG_PROJ_PATH = Path("F:/") / "4. Climate Datasets" / "Climate Vulnerability Tool" / "model inputs" / "Hazards" / "Extreme Weather"
+
+WIN_PRECIP_CHG_PROJ_PATH = Path("F:/") / "4. Climate Datasets" / "Climate Vulnerability Tool" / "model inputs" / "Hazards" / "Extreme Weather"
+
+
+
